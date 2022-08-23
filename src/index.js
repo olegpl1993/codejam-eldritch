0a134c6ff0266f225fd1b96dd7a2aa32114df7d5
olegpl1993 <olegpl1993@gmail.com>
2022-08-23 17:17:52 +0300

    feat: changing the data storage structure

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 import './styles/index.scss';
 import greenCards from './assets/MythicCards/green/index';
-import redCards from './assets/MythicCards/brown/index';
+import brownCards from './assets/MythicCards/brown/index';
 import blueCards from './assets/MythicCards/blue/index';
 import difficulties from './data/difficulties';
 import ancients from './data/ancients';
 import greenCardsData from './data/mythicCards/green/index';
-import redCardsData from './data/mythicCards/brown/index';
+import brownCardsData from './data/mythicCards/brown/index';
 import blueCardsData from './data/mythicCards/blue/index';
 import cardBack from './assets/mythicCardBackground.png';
 
-let cardDeck = []; //колода карт ссылки
-let cardColor = []; //колода карт цвета
-
+let cardDeck = [[], [], []]; //колода карт ссылки
 
 //трекер (обновляет текущее состояние колоды)-------------------------------
 const tracker = () => {
     let stats = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     //подсчитывает количество карт каждого цвета
-    for (let i = 0; i < cardColor[0].length; i++) {
-        if (cardColor[0][i] === 'green') stats[0] += 1;
-        if (cardColor[0][i] === 'red') stats[1] += 1;
-        if (cardColor[0][i] === 'blue') stats[2] += 1;
+    for (let i = 0; i < cardDeck[0].length; i++) {
+        if (cardDeck[0][i].color === 'green') stats[0] += 1;
+        if (cardDeck[0][i].color === 'brown') stats[1] += 1;
+        if (cardDeck[0][i].color === 'blue') stats[2] += 1;
     }
-    for (let i = 0; i < cardColor[1].length; i++) {
-        if (cardColor[1][i] === 'green') stats[3] += 1;
-        if (cardColor[1][i] === 'red') stats[4] += 1;
-        if (cardColor[1][i] === 'blue') stats[5] += 1;
+    for (let i = 0; i < cardDeck[1].length; i++) {
+        if (cardDeck[1][i].color === 'green') stats[3] += 1;
+        if (cardDeck[1][i].color === 'brown') stats[4] += 1;
+        if (cardDeck[1][i].color === 'blue') stats[5] += 1;
     }
-    for (let i = 0; i < cardColor[2].length; i++) {
-        if (cardColor[2][i] === 'green') stats[6] += 1;
-        if (cardColor[2][i] === 'red') stats[7] += 1;
-        if (cardColor[2][i] === 'blue') stats[8] += 1;
+    for (let i = 0; i < cardDeck[2].length; i++) {
+        if (cardDeck[2][i].color === 'green') stats[6] += 1;
+        if (cardDeck[2][i].color === 'brown') stats[7] += 1;
+        if (cardDeck[2][i].color === 'blue') stats[8] += 1;
     }
     //заполняет ячейки в документе
     document.getElementById('stat1').textContent = stats[0];
@@ -53,18 +51,16 @@ const takeNextCard = () => {
     console.log(cardDeck)
     if (cardDeck[0].length > 0) { //проверка наличия карт в массиве
         changeCard(cardDeck[0].pop()); //меняет кущую карту на экране и удаляет из массива
-        cardColor[0].pop(); //удаляет карту из массива цветов
         tracker(); //обновляет текущее состояние колоды
     } else if (cardDeck[1].length > 0) {
         changeCard(cardDeck[1].pop());
-        cardColor[1].pop();
         tracker();
     } else if (cardDeck[2].length > 0) {
         changeCard(cardDeck[2].pop());
-        cardColor[2].pop();
         tracker();
-    } else { //если карты закончились
-        deck.style.background = `none`;
+    }
+    if (cardDeck[0].length === 0 && cardDeck[1].length === 0 && cardDeck[2].length === 0) { // карты закончились
+        deck.style.background = `none`; //убирает картинку колоды
     }
 }
 //---------------------------------------------------------------------------
@@ -72,7 +68,7 @@ const takeNextCard = () => {
 //меняет текущую карту на экране --------------------------------------------
 const changeCard = (img) => {
     const card = document.querySelector('.right__card');
-    card.style.background = `url(${img})`;
+    card.style.background = `url(${img.cardFace})`;
     card.style.backgroundPosition = 'center';
     card.style.backgroundRepeat = 'no-repeat';
     card.style.backgroundSize = 'contain';
@@ -168,13 +164,45 @@ mixBtn.addEventListener('click', () => {
     gameAncients = '';
     deck.style.background = `url(${cardBack})`; //добавляет картинку полной калоды карт
     document.querySelector('.right__card').style.background = `none`; //убирает картинку карты с предыдущей игры
+    console.log(cardDeck)
 })
 
 
-//доделать функцию замеса колоды карт
 const mixDeck = () => {
     console.log(gameAncients, gameDifficult);
-    cardDeck = [[greenCards.green12, redCards.brown11, redCards.brown14], [redCards.brown6, greenCards.green14], [blueCards.blue10]];
-    cardColor = [['green', 'red', 'red'], ['red', 'green'], ['blue']];
+
+    if (gameAncients === 'azathoth') { // первый древний / легкий уровень
+        //первая стадия
+        for (let i = 0; i < ancients[0].firstStage.greenCards; i++) {
+            cardDeck[0].push(greenCardsData[0]); //добавляем карту в массив
+        }
+        for (let i = 0; i < ancients[0].firstStage.brownCards; i++) {
+            cardDeck[0].push(brownCardsData[0]);
+        }
+        for (let i = 0; i < ancients[0].firstStage.blueCards; i++) {
+            cardDeck[0].push(blueCardsData[0]);
+        }
+        //вторая стадия
+        for (let i = 0; i < ancients[0].secondStage.greenCards; i++) {
+            cardDeck[1].push(greenCardsData[0]);
+        }
+        for (let i = 0; i < ancients[0].secondStage.brownCards; i++) {
+            cardDeck[1].push(brownCardsData[0]);
+        }
+        for (let i = 0; i < ancients[0].secondStage.blueCards; i++) {
+            cardDeck[1].push(blueCardsData[0]);
+        }
+        //третья стадия
+        for (let i = 0; i < ancients[0].thirdStage.greenCards; i++) {
+            cardDeck[2].push(greenCardsData[0]);
+        }
+        for (let i = 0; i < ancients[0].thirdStage.brownCards; i++) {
+            cardDeck[2].push(brownCardsData[0]);
+        }
+        for (let i = 0; i < ancients[0].thirdStage.blueCards; i++) {
+            cardDeck[2].push(blueCardsData[0]);
+        }
+    }
+
 }
 //--------------------------------------------------------------------------
